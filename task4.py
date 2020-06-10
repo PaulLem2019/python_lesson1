ef53d00c25a1ff618655d2301c915099c215c6dd
PaulLem2019 <46632436+PaulLem2019@users.noreply.github.com>
2020-06-11 00:47:12 +0300

    Add files via upload
    First task on python course

---
+++ task4.py
@@ -0,0 +1,17 @@
+"""
+4. Пользователь вводит целое положительное число. Найдите самую большую цифру в числе.
+Для решения используйте цикл while и арифметические операции.
+"""
+
+number = int (input ("Введите целое положительное число "))
+
+beg_number = number
+maximal = number % 10
+
+while number > 0:
+    if (number % 10) > maximal:
+        maximal = number % 10
+
+    number //= 10
+
+print (f'{maximal} - максимальное цифра в числе {beg_number}')
